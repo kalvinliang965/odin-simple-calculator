a31d805d940ec8332704de6c4b1f6b7388e214e9
Kalvin Liang Wu <kalvinliang123@gmail.com>
2024-06-05 21:12:13 -0400

    completed basic feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 
 const screen = document.querySelector(".screen");
-let expression = "";
+let expression = [];
+let showResult = false;
 
 main();
 
@@ -9,11 +10,22 @@ function main() {
     buttons.forEach((button) => {
         button.addEventListener("click", number_handler);
     });
-
-
     const clearButton = document.querySelector("#clear");
     clearButton.removeEventListener("click", number_handler);
     clearButton.addEventListener("click", clear);
+
+    const deleteButton = document.querySelector("#delete");
+    deleteButton.removeEventListener("click", number_handler);
+    deleteButton.addEventListener("click", undo);
+
+    const equalButton = document.querySelector("#equal");
+    equalButton.removeEventListener("click", number_handler);
+    equalButton.addEventListener("click", () => {
+        const ret = calculate(expression);
+        clear();
+        enter2screen(ret);
+        showResult = true;
+    });
 }
 
 function isOperator(op) {
@@ -22,31 +34,50 @@ function isOperator(op) {
 }
 
 function isNum(text) {
-    return (text >= '0' && text <= '9');
+    return !isNaN(Number(text));
+}
+
+function precedence(op) {
+    switch(op) {
+        case '$':
+            return 0;
+        case '(':
+            return 1;
+        case '+':
+        case '-':
+            return 2;
+        case '*':
+        case '/':
+        case '%':
+            return 3;
+        default:
+            alert(`Error: precedence recieve: ${op}`);
+            return -1; 
+    }
 }
 
 function number_handler(event) {
     enter2screen(event.target.textContent);
 }
 
-
 function enter2screen(newText) {
-    const delimiter = /[+\-*\/()%]/;
-    const arr = expression.split(delimiter);
-    console.log(`expression: ${arr}`);
-    const lastElement = arr.at(-1);
-    if (newText == '.') {
-        // not allow multiple decimals
-        if (lastElement.includes('.')) return;
-    }  else {
-        if (isOperator(newText)) {
-            if (expression == "") return; // not allow to start with operator
-            if (isOperator(newText) && isOperator(lastElement)) {
-                return; // not allow to have mutiple operator at once
-            }
-        }
+    if (showResult) return; // showing the result, not allow to enter anything
+    const lastElement = expression.at(-1);
+
+    if (expression.length > 0) {
+        if (newText == '.' && lastElement.includes('.'))  return;
+        else if (isOperator(newText)) {
+            if (isOperator(expression.at(-2))) return;
+            expression.push(newText);
+            expression.push("");
+        } else expression[expression.length - 1] = lastElement + newText;
+    } 
+    // empty expression
+    else {
+        if (isOperator(newText)) return; // not allow to start with operator
+        expression.push(newText);
     }
-    expression += newText;
+    
     let line = screen.lastElementChild;
     if (line == null) {
         line = newLine();
@@ -70,46 +101,135 @@ function clear() {
         screen.removeChild(screen.firstElementChild);
     }
     expression = [];
+    showResult = false;
 }
 
-function add(a,b) {
+function operate(a,b, op) {
+    switch(op) {
+        case '+':
+            // console.log(`a: ${a}`);
+            // console.log(`b: ${b}`);
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (a === 0 && b === 0)
+                return "ERROR";
+            if (a > 0 && b === 0) 
+                return "INFINITY";
+            if (a < 0 && b === 0) 
+                return "-INFINITY";
 
-}
-
-function sub(a,b) {
-
-}
-
-function mult(a,b) {
-
-}
-
-function div(a,b) {
-
-}
-
-function mod(a,b) {
-
-}
-
-function operate(a,b) {
-
+            return a / b;
+        case '%':
+            return a % b;
+        default:
+            alert(`Error in operate: op:${op}`);
+    }
 }
 
 function undo() {
+    if (showResult) return; // showing the result, not allow to remove anything
+    const lastLine = screen.lastElementChild;
+    if (lastLine == null) return; // element not exist
+    let text = lastLine.textContent;
+    text = text.substring(0, text.length - 1);
+    if (text == "") {
+        screen.removeChild(lastLine);
+    } else lastLine.textContent = text; 
 
 }
 
 function calculate(expression) {
-
+    // the expression is invalid
+    if (!validParentheses(expression)) {
+        clear();
+        enter2screen("ERROR");
+        return;
+    }
+    const postfix = infix2postfix(expression);
+    const N = postfix.length;
+    stack = [];
+    let token, a, b, c, ret;
+    for (let i = 0; i < N; i++) {
+        token = postfix[i];
+        if (isNum(token)) {
+            stack.push(token);
+        } else if (isOperator(token)) {
+            b = parseFloat(stack.pop());
+            if (isNaN(b)) return "ERROR";
+            a = parseFloat(stack.pop());
+            if (isNaN(a)) return "ERROR";
+            c = operate(a, b, token);
+            if (typeof(c) == "string") return c; // error
+            stack.push(c + "");
+        } else {
+            alert(`ERROR in myEval: token is invalid: ${token}`);
+        }
+    }
+    console.log(`stack: ${stack}`);
+    ret = parseFloat(stack.pop());
+    if (Number.isInteger(ret)) {
+        return parseInt(ret);
+    } else return ret.toFixed(4);
 }
 
-function verify() {
-
+function validParentheses(expression) {
+    stack = [];
+    let token;
+    let stackElement;
+    const N = expression.length;
+    for (let i = 0; i < N; i++) {
+        token = expression[i];
+        if (token == '(')   stack.push(token);
+        else if (token == ')') {
+            if (stack.length == 0) return false;
+            stackElement = stack.pop();
+            if (stackElement != '(') {
+                alert(`stack contain invalid token: ${stackElement}`);
+                return false;
+            }
+        }
+    }
+    if (stack.length != 0) return false;
+    return true;
 }
 
-function infix2postfix(expression) {
-
+function infix2postfix(infix) {
+    let postfix = [];
+    let opStack = [];
+    let topOp; // top operator
+    opStack.push('$');
+    const N = infix.length;
+    let token;
+    for (let i = 0; i < N; i++) {
+        token = infix[i];
+        console.log(`token: ${token}`);
+        console.log(`postfix: ${postfix}`)
+        if (isNum(token)) postfix.push(token);
+        else if (token == '(') opStack.push(token);
+        else if (token == ')') {
+            topOp = opStack.pop();
+            while (topOp != '(') {
+                postfix.push(topOp); 
+                topOp = opStack.pop();
+            }
+        } else if (isOperator(token)) {
+            topOp = opStack[opStack.length - 1];
+            while (precedence(token) <= precedence(topOp)) {
+                postfix.push(topOp);
+                opStack.pop();
+                topOp = opStack[opStack.length - 1];
+            }
+            opStack.push(token);
+        }
+    }
+    topOp = opStack.pop();
+    while (topOp != '$') {
+        postfix.push(topOp);
+        topOp = opStack.pop();
+    }
+    return postfix;
 }
-
-// handlers...
